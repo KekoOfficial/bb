939a1986e1be4081ab1f015c4c8954f1218cd000
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-20 16:32:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const flowers = document.querySelectorAll('.flower');
-
-flowers.forEach(flower => {
-  const leftPos = Math.random() * window.innerWidth;
-  flower.style.left = `${leftPos}px`;
-  flower.style.animationDuration = `${3 + Math.random() * 2}s`;
-});
